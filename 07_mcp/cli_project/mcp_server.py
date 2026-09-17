6820dcd8e43f817c407072729169ed679a52b0ee
Nyukaa <annashitikova@gmail.com>
2026-09-17 11:18:53 +0300

    Add prompt to reformat documents in md format in server

--- 07_mcp/cli_project/mcp_server.py
+++ 07_mcp/cli_project/mcp_server.py
@@ -1,4 +1,5 @@
 from mcp.server.fastmcp import FastMCP
+from mcp.server.fastmcp.prompts import base 
 from pydantic import Field
 mcp = FastMCP("DocumentMCP", log_level="ERROR")
 # uv run mcp dev mcp_server.py
@@ -76,7 +77,26 @@ def fetch_doc(doc_id: str) -> str:
     if doc_id not in docs:
         raise ValueError(f"Doc with id {doc_id} not found")
     return docs[doc_id]
-# TODO: Write a prompt to rewrite a doc in markdown format
+# Write a prompt to rewrite a doc in markdown format
+@mcp.prompt(
+    name="format",
+    description="Rewrite a document in markdown format.",
+)
+def format_document(
+    doc_id: str = Field(description="Id of the document to format"),
+) -> list[base.Message]:
+    prompt = f"""
+
+    Your goal is to reformat a document to be written with markdown syntax. 
+    The id of the document you need to reformat is:
+    <document_id>
+    {doc_id}
+    </document_id>
+    Add in headers, bullet points, tables, etc as necessary. Feel free to add in extra formatting.
+    Use the 'edit_document' tool to edit the document. After the document has been reformatted...
+    """
+
+    return [base.UserMessage(prompt)]
 # TODO: Write a prompt to summarize a doc
 
 
